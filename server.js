274334892125e19d3f7920beca36ff5311a60f68
Germaine Pannell <me@germainep.com>
2020-05-06 19:40:02 -0400

    created the initial database schema

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
 const express = require('express')
-
+const helmet = require('helmet')
 const server = express()
+
+server.use(express.json())
+server.use(helmet())
 
 server.listen(process.env.PORT, () => {
   console.log(`server listening on port ${ process.env.PORT }`)
